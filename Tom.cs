ab27ec58d61e401ebb1febf3bce0ffb3950bd10c
Tom Kral <thjkral@gmail.com>
2022-01-05 15:18:17 +0100

    Tom's eerste commit

---
+++ Tom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class TestClass
+{
+	public Class1()
+	{
+		static void Main(string[] args)
+        {
+			Console.Writeline("Ik ben naar een nieuwe branch. Groet, Tom");
+        }
+
+	}
+}
